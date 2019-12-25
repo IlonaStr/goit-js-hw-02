bc3fd4134059e60e5c4f6a1403b1a56d9762ac35
ilona <hellosidney17@gmail.com>
2019-12-25 10:10:28 +0200

    fixings

--- tasks/task-6.js
+++ tasks/task-6.js
@@ -9,10 +9,12 @@ do{
     numbers.push(Number(input));
 } while (input !== null);
 
-if (numbers.length){
+if (numbers.length, input !== null){
     for (let num of numbers){
     total += num;
-}
+} 
+console.log(`Общая сумма чисел равна ${total}`);
+} else if (input === null){
+    console.log('Попробуйте еще раз ввести число');
 }
 
-console.log(`Общая сумма чисел равна ${total}`);
